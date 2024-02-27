c3d45515c1438182980aee1a253e32f60e41448a
Omochice <mochice.mls.ntl@gmail.com>
2024-02-27 23:43:05 +0900

    build: add build script

---
+++ build.mjs
@@ -0,0 +1,39 @@
+import * as esbuild from "esbuild";
+import { readFileSync } from "fs";
+import { ensure, is } from "unknownutil";
+
+const userScript = ({ name, version, description, author }) => ({
+  name: "userScript",
+  setup(build) {
+    build.initialOptions.banner = {
+      js: [
+        "// ==UserScript==",
+        `// @name ${name}`,
+        `// @version ${version}`,
+        `// @description ${description}`,
+        `// @author ${author}`,
+        `// @match https://github.com/notifications*`,
+        "// ==/UserScript==",
+      ].join("\n"),
+    };
+  },
+});
+
+const isPackage = is.ObjectOf({
+  name: is.String,
+  version: is.String,
+  description: is.String,
+  author: is.String,
+});
+
+esbuild.build({
+  entryPoints: ["src/index.ts"],
+  bundle: true,
+  minify: true,
+  outfile: "dist/index.user.js",
+  platform: "browser",
+  format: "esm",
+  plugins: [
+    userScript(ensure(JSON.parse(readFileSync("package.json")), isPackage)),
+  ],
+});
